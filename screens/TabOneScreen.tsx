13c9c228b26933c8ad043fb4f10a5e05d420d271
perlinleo <perlinleo@github.com>
2022-10-08 13:47:07 +0300

    mask

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,4 +1,7 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Image } from 'react-native';
+
+
+import MaskedView from "@react-native-masked-view/masked-view";
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -7,8 +10,48 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <MaskedView
+        style={{ flex: 1, flexDirection: "row", height: "100%" }}
+        maskElement={
+          <View
+            style={{
+              // Transparent background because mask is based off alpha channel.
+              backgroundColor: "transparent",
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              style={{ width: 200, height: 200 }}
+              source={{
+                uri: "https://pngimg.com/uploads/love/love_PNG85.png",
+              }}
+            />
+          </View>
+        }
+      >
+        {/* Shows behind the mask, you can put anything here, such as an image */}
+        <Image
+          style={{ width: 200, height: 400 }}
+          source={{
+            uri: "https://c.tenor.com/wsChytFfrS4AAAAM/monki-flip-monkey.gif",
+          }}
+        />
+      </MaskedView>
+      <Text style={styles.title}>Tab 1</Text>
+
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
+      <Image
+        style={{ width: 100, height: 100 }}
+        source={{
+          uri: "https://c.tenor.com/wsChytFfrS4AAAAM/monki-flip-monkey.gif",
+        }}
+      />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
   );
