66ea626b5437a48e031e954e1d0fa6675c9994ad
perlinleo <perlinleo@github.com>
2022-11-23 06:08:05 +0300

    🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢🤢->->->->->

--- components/feed/FeedCard.tsx
+++ components/feed/FeedCard.tsx
@@ -1,9 +1,10 @@
 import { View } from '../base/Themed';
-import { Image, Pressable, StyleSheet } from 'react-native';
+import { Image, Pressable, StyleSheet, Text } from 'react-native';
 import React from 'react';
 import { Colors } from '../../styles';
 import { AntDesign } from '@expo/vector-icons';
 import { LikeFeedCardButton } from './LikeFeedCardButton';
+import { IPost } from '../../reducers/posts/post';
 
 const styles = StyleSheet.create({
     image: { width: 170, height: 240, resizeMode: 'cover', borderRadius: 14 },
@@ -16,10 +17,10 @@ const styles = StyleSheet.create({
 });
 
 export interface FeedCardProps {
-    previewSrc: string;
     id: number;
     onPress?: () => void;
     hasLikeButton: boolean;
+    post: IPost;
 }
 
 export const FeedCard = (props: FeedCardProps) => (
@@ -27,11 +28,27 @@ export const FeedCard = (props: FeedCardProps) => (
         <Pressable onPress={props.onPress}>
             <Image
                 style={styles.image}
-                source={{
-                    uri: `data:image/jpg;base64,${props.previewSrc}`
-                }}
+                source={(() => {
+                    console.log(props.post.look?.img_path);
+                    if (props.post.previews_paths) {
+                        return {
+                            uri: `${
+                                props.post.previews_paths.isEmpty()
+                                    ? `http://leonidperl.in/${props.post.previews[0]}`
+                                    : `http://leonidperl.in/${props.post.previews[0]}`
+                            }`
+                        };
+                    } else {
+                        return {
+                            uri: `http://leonidperl.in/${props.post.look?.img_path}`
+                        };
+                    }
+                })()}
             />
-            <LikeFeedCardButton />
+            <LikeFeedCardButton
+                id={props.post.id}
+                currentLikes={props.post.likes}
+            />
         </Pressable>
     </View>
 );
