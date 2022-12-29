78d98f5a77b0d31d345b2d2bf65253de01ee4fed
Vladimir Lunkin <v.lunkin@vk.team>
2022-12-29 18:00:53 +0300

    ip

--- network/const.ts
+++ network/const.ts
@@ -1,8 +1,11 @@
-export const FileHost = 'http://leonidperl.in:5001/';
+// const host = 'leonidperl.in'
+const host = '185.130.114.65'
 
-export const ImgHost = 'http://leonidperl.in:80/';
+export const FileHost = `http://${host}:5001/`;
 
-export const baseURL = 'http://leonidperl.in:3001/api/v1';
+export const ImgHost = `http://${host}:80/`;
+
+export const baseURL = `http://${host}:3001/api/v1`;
 
 export function getUri(path: string) {
     return ImgHost + path;
