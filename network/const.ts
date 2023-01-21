7c37cd9bc15cd7714de2af0350dadd89271e79e2
leonidperlin <leonidperlin@leonidperlins-MacBook-Pro.local>
2023-01-21 09:59:20 -0800

    fix: port

--- network/const.ts
+++ network/const.ts
@@ -1,11 +1,11 @@
-const host = 'leonidperl.in'
+const host = 'leonidperl.in';
 // const host = '185.130.114.65'
 
-export const FileHost = `http://${host}:5001/`;
+export const FileHost = `http://${host}:80/`;
 
 export const ImgHost = `http://${host}:80/`;
 
-export const baseURL = `http://${host}:3001/api/v1`;
+export const baseURL = `http://${host}:80/api/v1`;
 
 export function getUri(path: string) {
     return ImgHost + path;
