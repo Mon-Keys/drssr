39f2c469a6592dfa5432b25f5ee573b6d9c4dcd0
Andrey123815 <ivredomon@gmail.com>
2023-03-02 13:24:26 +0300

    refact: rewrite readme, add instructions, remove trash pre-commit keys

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,17 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'type-enum': [
-            2,
-            'always',
-            [
-                'ci',
-                'docs',
-                'fix',
-                'refact',
-                'revert',
-                'code'
-            ]
-        ]
+        'type-enum': [2, 'always', ['fix', 'ci', 'refact', 'code']]
     }
 };
